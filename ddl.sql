9bf68c7d39ac5c94ba9e94afa71b024600927a15
ananthkrk <30938917+ananthkrk@users.noreply.github.com>
2020-07-18 15:39:12 -0700

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,8 +1,26 @@
 
---BASIC SUDOKU TABLE
+--Basic Sudoku Table
 CREATE TABLE SUDOKU
 (
-RW_NO NUMBER(1),
-CL_NO NUMBER(1),
-VL_VL NUMBER(1)
+  RW_NO NUMBER(1),
+  CL_NO NUMBER(1),
+  VL_VL NUMBER(1),
+  Constraint PK_SUDOKU PRIMARY KEY (RW_NO, CL_NO)
+);
+
+--User Defined Block(s) 
+CREATE TABLE SUDOKU_BLOCK
+(
+  RW_NO NUMBER(1),
+  CL_NO NUMBER(1),
+  BLKNO NUMBER(1) --specifies which block tht particular row/column belongs
+);
+
+-- All Possible values that a Sudoku Field can take
+CREATE TABLE SUDOKU_ALLPOSSIBLE_VALUES
+(
+  RW_NO NUMBER(1),
+  CL_NO NUMBER(1),
+  BLKNO NUMBER(1), 
+  VL_VL NUMBER(1)
 );
